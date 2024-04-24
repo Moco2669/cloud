662c2b1a8e79e9e6c71be91d7fe833dcc8d10524
Lundjo <lukadjelic529@gmail.com>
2024-04-24 11:53:18 +0200

    Fixed frontend errors

--- client/src/pages/post/view/Post.tsx
+++ client/src/pages/post/view/Post.tsx
@@ -3,13 +3,14 @@ import { useParams } from "react-router-dom";
 
 import Navbar from "../../../components/navbar/Navbar";
 import Post from "../../../components/post/view/Post";
+import ISearchBarQueryProps from "../../../interfaces/search/ISearchBarQuery";
 
-const PostPage: React.FC = () => {
+const PostPage: React.FC <ISearchBarQueryProps>= ({query, setQuery}) => {
   const { id } = useParams<{ id: string }>();
 
   return (
     <>
-      <Navbar />
+      <Navbar query={query} setQuery={setQuery}/>
       <div className="flex justify-center mt-12">
         <div className="w-full max-w-screen-lg">
           <Post postId={id ?? ""} />
