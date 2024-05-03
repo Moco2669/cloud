80eb35e446a4f244f959bb46e8b49fa45ce2bfa5
Pinki223 <116367165+Pinki223@users.noreply.github.com>
2024-05-03 20:16:10 +0200

    ima dosta

--- backend/HealthMonitor/WorkerRole.cs
+++ backend/HealthMonitor/WorkerRole.cs
@@ -7,10 +7,16 @@ using System.ServiceModel;
 using System.Threading;
 using System.Threading.Tasks;
 using Common;
+using Common.cloud.queue;
 using Microsoft.WindowsAzure;
 using Microsoft.WindowsAzure.Diagnostics;
 using Microsoft.WindowsAzure.ServiceRuntime;
 using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Queue;
+using Microsoft.WindowsAzure.Storage.Table;
+using RedditDataRepository.queues;
+using RedditDataRepository.tables;
+using RedditDataRepository.tables.entities;
 
 namespace HealthMonitor
 {
@@ -19,8 +25,10 @@ namespace HealthMonitor
         private readonly CancellationTokenSource cancellationTokenSource = new CancellationTokenSource();
         private readonly ManualResetEvent runCompleteEvent = new ManualResetEvent(false);
 
-        private IHealthMonitoring proxy;
-        private string redditServiceEndpoint;
+        private List<string> redditServiceEndpoints = new List<string>();
+        private static HealthCheckRepository repository = new HealthCheckRepository();
+        private static AdminToolServer AdminToolServer;
+        private int instanceIndex;
 
         public override void Run()
         {
@@ -38,15 +46,21 @@ namespace HealthMonitor
 
         public override bool OnStart()
         {
-            // dizvucem id instance da bi jedan healthMonitor pratio jedan RedditService a drugi drugi
+
             string roleId = RoleEnvironment.CurrentRoleInstance.Id;
             int startIndex = roleId.LastIndexOf("_IN_") + 4;
             int endIndex = roleId.Length;
             string instanceIndexStr = roleId.Substring(startIndex, endIndex - startIndex);
+            instanceIndex = int.Parse(instanceIndexStr);
 
-            int instanceIndex = int.Parse(instanceIndexStr);
-            int port = 6000 + instanceIndex;
-            redditServiceEndpoint = $"net.tcp://localhost:{port}/HealthMonitoring";
+            int portBase = 6000;
+            int redditServiceCount = 2;
+            for (int i = 0; i < redditServiceCount; i++)
+            {
+                int port = portBase + i;
+                string endpoint = $"net.tcp://localhost:{port}/HealthMonitoring";
+                redditServiceEndpoints.Add(endpoint);
+            }
 
             // Set the maximum number of concurrent connections
             ServicePointManager.DefaultConnectionLimit = 12;
@@ -55,6 +69,9 @@ namespace HealthMonitor
             // see the MSDN topic at https://go.microsoft.com/fwlink/?LinkId=166357.
 
             bool result = base.OnStart();
+
+            AdminToolServer = new AdminToolServer();
+            AdminToolServer.Open();
 
             Trace.TraceInformation("HealthMonitor has been started");
 
@@ -70,33 +87,68 @@ namespace HealthMonitor
 
             base.OnStop();
 
+            AdminToolServer.Close();
+
             Trace.TraceInformation("HealthMonitor has stopped");
         }
 
-        public void Connect()
+        public void Connect(string endpoint)
         {
             var binding = new NetTcpBinding();
-            ChannelFactory<IHealthMonitoring> factory = new ChannelFactory<IHealthMonitoring>(binding, new EndpointAddress(redditServiceEndpoint));
-            proxy = factory.CreateChannel();
+            ChannelFactory<IHealthMonitoring> factory = new ChannelFactory<IHealthMonitoring>(binding, new EndpointAddress(endpoint));
+            IHealthMonitoring proxy = factory.CreateChannel();
+            CheckServiceHealth(proxy);
         }
 
         private async Task RunAsync(CancellationToken cancellationToken)
         {
-            Connect();
 
             while (!cancellationToken.IsCancellationRequested)
             {
-                try
+                if(instanceIndex == 0)
                 {
-                    proxy.IAmAlive();
-                    Trace.TraceInformation("Service is alive.");
+                    foreach (var endpoint in redditServiceEndpoints)
+                    {
+                        Connect(endpoint);
+                    }
                 }
-                catch
-                {
-                    Trace.TraceWarning("Service not alive anymore!");
-                }
-                await Task.Delay(5000);
+
+                await Task.Delay(new Random().Next(1000, 5001), cancellationToken);
             }
+        }
+
+        private void CheckServiceHealth(IHealthMonitoring proxy)
+        {
+            try
+            {
+                proxy.IAmAlive();
+                LogHealthCheck("OK");
+                Trace.TraceInformation("Service is alive.");
+            }
+            catch
+            {
+                LogHealthCheck("NOT_OK");
+                Trace.TraceWarning("Service not alive anymore!");
+                //CloudQueue queue = AzureQueueHelper.GetQueue("adminnotificationqueue");
+                //AdminNotificationQueue.EnqueueMessage(queue, "Alert admin emails!");
+            }
+        }
+
+        private void LogHealthCheck(string status)
+        {
+            string service = "";
+            if(instanceIndex == 0)
+            {
+                service = "Reddit";
+            }
+            if(instanceIndex == 1)
+            {
+                service = "Notification";
+            }
+
+            DateTime timestamp = DateTime.UtcNow;
+            HealthCheck healthCheckEntity = new HealthCheck(timestamp.ToString("yyyyMMddHHmmssfff"), status, service);
+            repository.Create(healthCheckEntity);
         }
     }
 }
---
+++ backend/HealthStatus/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using RedditDataRepository.tables;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HealthStatus.Controllers
+{
+    public class HomeController : Controller
+    {
+        private static HealthCheckRepository _healthCheckRepository = new HealthCheckRepository();
+
+        public ActionResult Index()
+        {
+            DateTime now = DateTime.UtcNow;
+            DateTime oneHourAgo = now.AddHours(-1);
+            DateTime twentyFourHoursAgo = now.AddHours(-24);
+
+            int oneHourOkCount = _healthCheckRepository.GetOkCheckCount(oneHourAgo, now);
+            int oneHourTotalCount = _healthCheckRepository.GetCheckCount(oneHourAgo, now);
+            int twentyFourHoursOkCount = _healthCheckRepository.GetOkCheckCount(twentyFourHoursAgo, now);
+            int twentyFourHoursTotalCount = _healthCheckRepository.GetCheckCount(twentyFourHoursAgo, now);
+
+            ViewBag.OneHourOkCount = oneHourOkCount;
+            ViewBag.OneHourTotalCount = oneHourTotalCount;
+            ViewBag.TwentyFourHoursOkCount = twentyFourHoursOkCount;
+            ViewBag.TwentyFourHoursTotalCount = twentyFourHoursTotalCount;
+            ViewBag.HealthCheckRepository = _healthCheckRepository;
+
+            return View();
+        }
+
+    }
+}
